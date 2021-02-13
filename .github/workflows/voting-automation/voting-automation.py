509b357c10ebc6b28e96a96b229ab90c817df7e6
Donnie Earnest <earnestdl@ornl.gov>
2021-02-12 23:12:57 -0600

    refactor

--- .github/workflows/voting-automation/voting-automation.py
+++ .github/workflows/voting-automation/voting-automation.py
@@ -8,33 +8,26 @@ user = os.environ.get('user')
 passwd = os.environ.get('pass')
 
 def issues():
-
     url = "%s/repos/%s/%s/issues" % (api,org,repo) 
     headers = {'Accept': 'application/vnd.github.v3+json'}
-
-    resp = requests.get(url, headers=headers)
-
-    if resp.status_code != 200:
-        raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-
-    return resp.json()
+    return request(url,headers)
 
 def reactions(issue):
-
-    print('\nReactions:')
-
     url = "%s/repos/%s/%s/issues/%s/reactions" % (api,org,repo,issue) 
     headers = {'Accept': 'application/vnd.github.squirrel-girl-preview+json'}
+    return request(url,headers)
 
+def request(url,headers):
     resp = requests.get(url, headers=headers)
     if resp.status_code != 200:
         raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-    for reaction in resp.json():
-        print('reaction: {}'.format(reaction['reaction']))
-
+    return resp.json()
 
 
 if __name__ == "__main__":
 
     for issue in issues():
         print('number: {} title: {}'.format(issue['number'], issue['title']))
+
+    for reaction in reactions(1):    
+        print('reaction: {}'.format(reaction['content']))
