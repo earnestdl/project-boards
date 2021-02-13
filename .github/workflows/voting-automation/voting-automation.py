2378bae218712564e4c9f692fe27ea209533a604
Donnie Earnest <earnestdl@ornl.gov>
2021-02-12 22:56:56 -0600

    added reactions

--- .github/workflows/voting-automation/voting-automation.py
+++ .github/workflows/voting-automation/voting-automation.py
@@ -7,19 +7,34 @@ api = "https://api.github.com"
 user = os.environ.get('user')
 passwd = os.environ.get('pass')
 
-def get_issues():
-
-    print('\nIssues:')
+def issues():
 
     url = "%s/repos/%s/%s/issues" % (api,org,repo) 
     headers = {'Accept': 'application/vnd.github.v3+json'}
 
     resp = requests.get(url, headers=headers)
+
     if resp.status_code != 200:
         raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-    for issue in resp.json():
-        print('number: {} title: {}'.format(issue['number'], issue['title']))
+
+    return resp.json()
+
+def reactions(issue):
+
+    print('\nReactions:')
+
+    url = "%s/repos/%s/%s/issues/%s/reactions" % (api,org,repo,issue) 
+    headers = {'Accept': 'application/vnd.github.squirrel-girl-preview+json'}
+
+    resp = requests.get(url, headers=headers)
+    if resp.status_code != 200:
+        raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+    for reaction in resp.json():
+        print('reaction: {}'.format(reaction['reaction']))
+
 
 
 if __name__ == "__main__":
-    get_issues()
+
+    for issue in get_issues():
+        print('number: {} title: {}'.format(issue['number'], issue['title']))
