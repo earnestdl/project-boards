4f57e18c478927a3bf99c2419bd3392000151315
Donnie Earnest <earnestdl@ornl.gov>
2021-02-20 10:29:46 -0600

    adds funtion to add label

--- .github/workflows/voting-automation/voting-automation.py
+++ .github/workflows/voting-automation/voting-automation.py
@@ -7,33 +7,46 @@ token = os.environ.get('token')
 def issues():
     url = "%s/repos/%s/%s/issues" % (api,org,repo) 
     headers = {'Accept': 'application/vnd.github.v3+json'}
-    return request(url,headers)
+    return get(url,headers)
 
 def reactions(issue):
     url = "%s/repos/%s/%s/issues/%s/reactions" % (api,org,repo,issue) 
     headers = {'Accept': 'application/vnd.github.squirrel-girl-preview+json'}
-    return request(url,headers)
+    return get(url,headers)
 
 def projects():
     url = "%s/repos/%s/%s/projects" % (api,org,repo) 
     headers = {'Accept': 'application/vnd.github.inertia-preview+json',
                 'Authorization': 'Bearer ' + token }
-    return request(url,headers)
+    return get(url,headers)
     
 def columns(project):
     url = "%s/projects/%s/columns" % (api,project) 
     headers = {'Accept': 'application/vnd.github.inertia-preview+json',
                 'Authorization': 'Bearer ' + token }
-    return request(url,headers)
+    return get(url,headers)
 
 def cards(column):
     url = "%s/projects/columns/%s/cards" % (api,column) 
     headers = {'Accept': 'application/vnd.github.inertia-preview+json',
                 'Authorization': 'Bearer ' + token }
-    return request(url,headers)
+    return get(url,headers)
 
-def request(url,headers):
+def add_label(issue):
+    url = "%s/repos/%s/%s/issues/%s/labels" % (api,org,repo,issue) 
+    headers = {'Accept': 'application/vnd.github.v3+json',
+                'Authorization': 'Bearer ' + token }
+    payload = '{"labels":["needs votes"]}'
+    return post(url,headers,payload)
+
+def get(url,headers):
     resp = requests.get(url, headers=headers)
+    if resp.status_code != 200:
+        raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+    return resp.json()
+
+def post(url,headers,payload):
+    resp = requests.post(url, headers=headers, data=payload)
     if resp.status_code != 200:
         raise ApiError('GET /tasks/ {}'.format(resp.status_code))
     return resp.json()
@@ -58,3 +71,18 @@ if __name__ == "__main__":
 
         print('Upvotes: %d' % uvote)
         print('Downvotes: %d' % dvote)
+
+        if uvote = 0 and dvote = 0:
+            print("no votes! adding label...")
+            add_label(issue)
+        if uvote > dvote:
+            diff = uvote-dvote
+            if diff > 2:
+                print("there are 2 more upvotes than downvotes. move to next column!")
+        elif dvote > uvote:
+            diff = dvote=uvote
+            if diff > 2:
+                print("there are 2 more downvotes than upvotes. close this issue!")
+
+
+
