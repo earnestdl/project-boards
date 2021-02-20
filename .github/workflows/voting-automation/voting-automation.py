5c266dc6d6cebb7a8697c4539c3bbec0c1ab7cdd
Donnie Earnest <earnestdl@ornl.gov>
2021-02-20 09:48:58 -0600

    refactor

--- .github/workflows/voting-automation/voting-automation.py
+++ .github/workflows/voting-automation/voting-automation.py
@@ -41,22 +41,16 @@ def request(url,headers):
 
 if __name__ == "__main__":
 
-
-    # get a list of cards in the "In Progress" column and add them to an array
-    # so we have a reference to them later when we need to move them to the next column
-
-
-    print('\nList cards in the In Progress column')
+    print('\nList cards in the In Progress column:')
     for card in cards(col_in_progress):  
 
         uvote=0
         dvote=0
         issue = card['content_url'].split('/')[-1]
+
         print("\nIssue: %s" % issue)
 
         for reaction in reactions(issue):
-            #print('reaction: {}'.format(reaction['content']))
-
             if reaction['content'] == '+1':
                 uvote=uvote+1
             elif reaction['content'] == '-1':
@@ -64,28 +58,3 @@ if __name__ == "__main__":
 
         print('Upvotes: %d' % uvote)
         print('Downvotes: %d' % dvote)
-
-
-            #print('id: {}\nnote: {}\nissue: {}'.format(card['id'], card['note'], card['content_url']))
-
-
-#    print('\nIssues:')
-#    for issue in issues():
-#        print('number: {}\ntitle: {}'.format(issue['number'], issue['title']))
-
-#    print('\nReactions:')
-#    for reaction in reactions(1):    
-#        print('reaction: {}'.format(reaction['content']))
-
-#    print('\nProjects:')
-#    for project in projects():
-#        print('id: {}\ntitle: {}'.format(project['id'], project['name']))
-
-#    print('\nColumns:')
-#    for column in columns(project_id):
-#        print('id: {}\nname: {}'.format(column['id'], column['name']))
-
-#    print('\nColumn 2:')
-#    for column in column(2):
-#        print('number: {}\ntitle: {}'.format(project['number'], project['name']))
-
